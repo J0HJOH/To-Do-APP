ec7c27d7020353f29fc36fb0c50eb70503ae61a0
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2024-12-12 14:08:13 +0100

    Make some changes based on feedback from session

--- app/components/taskScrollView.jsx
+++ app/components/taskScrollView.jsx
@@ -4,50 +4,56 @@ import TaskCard from "../components/taskCard";
 
 
 const TaskScrollView = ({
-    taskList,
-    isFavCard = false,
-    cardStyle,
-    secondComponent
+  taskList,
+  isFavCard = false,
+  cardStyle,
+  secondComponent
 }) => {
-   
-    return taskList.map(category => {
+  // Use SectionList to render a list of tasks
+  // renderSectionHeader, renderItem
+
+  // const sectionData = [
+  // {title: "Work", data: ["Task 1", "Task 2", "Task 3"]},
+  //   ]
+
+  return taskList.map(category => {
 
     //if favCard is true return a list of fav tasks
-       const listType = isFavCard 
-       ? category.tasks.filter(task => task.fav) 
-       : category.tasks;
+    const listType = isFavCard
+      ? category.tasks.filter(task => task.fav)
+      : category.tasks;
 
-        return (
-            <View key={category.id}>
+    return (
+      <View key={category.id}>
 
-                <FlatList
-                    data={listType}
-                    key={category.tasks.id}
-                    scrollEnabled={false}
-                    //render Category
-                    renderItem={({ item, index }) => {
-                        return (
+        <FlatList
+          data={listType}
+          key={category.tasks.id}
+          scrollEnabled={false}
+          //render Category
+          renderItem={({ item, index }) => {
+            return (
 
-                            <TaskCard
-                                // item.tasks returns the list of task in that category
-                                eachTask={item}
-                                categoryId={category.id}
-                                taskId={item.id}
-                                secondComponent={secondComponent}
-                                isFavCard={isFavCard}
-                                cardStyle={cardStyle}
-                                index={index}
-                            />
-                        )
+              <TaskCard
+                // item.tasks returns the list of task in that category
+                eachTask={item}
+                categoryId={category.id}
+                taskId={item.id}
+                secondComponent={secondComponent}
+                isFavCard={isFavCard}
+                cardStyle={cardStyle}
+                index={index}
+              />
+            )
 
-                    }
-                    }
+          }
+          }
 
-                />
-            </View>
-        )
-    }
+        />
+      </View>
     )
+  }
+  )
 
 
 }
